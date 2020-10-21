ea9733dff137084ef0371aab13b833f658b91a39
George Lewis <georgejeffreylewis@gmail.com>
2020-10-21 17:55:44 -0400

    update examples

--- example_test.go
+++ example_test.go
@@ -5,9 +5,11 @@ func ExampleBeep() {
 }
 
 func ExampleNotify() {
+	SetAppID("App ID")
 	Notify("Title", "MessageBody", "assets/information.png")
 }
 
 func ExampleAlert() {
+	SetAppID("App ID")
 	Alert("Title", "MessageBody", "assets/warning.png")
 }
